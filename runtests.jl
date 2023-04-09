ae755012cf8a6f8b2d29209a97010c525bcbe4e8
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-09 10:02:45 +0100

    Add files via upload

---
+++ runtests.jl
@@ -0,0 +1,18 @@
+using Test
+
+function print_evalfile(fdir, fname)
+    println("EVALUATING ", fname, ":\n*************")
+    result = evalfile(joinpath(fdir, fname))
+    println("*************\n")
+    return result
+end
+
+fdir, this_fname = splitdir(@__FILE__)
+fdir = joinpath(fdir, "../src")
+cd(fdir)
+
+for fname in readdir(".")
+    if endswith(fname, ".jl") && fname != this_fname
+        print_evalfile(fdir, fname)
+    end
+end
