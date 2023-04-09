821a20f0a05d8bc6342ef0368d00557e5eb42bc9
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-09 10:05:08 +0100

    Add files via upload

---
+++ LightML.jl
@@ -0,0 +1,133 @@
+__precompile__()
+module LightML
+
+using Gadfly
+using DataFrames
+using ForwardDiff
+using Distributions
+using PyCall
+using PyPlot
+using DataStructures
+using Distances
+using Clustering
+
+
+
+@pyimport sklearn.datasets as dat
+
+
+
+export 
+
+    test_LinearRegression, 
+    test_LogisticRegression, 
+
+    test_ClassificationTree,
+    test_RegressionTree,
+
+    test_GDA,
+    test_HMM,
+
+    test_kneast_regression,
+    test_kneast_classification,
+
+
+    test_label_propagation,
+
+    test_LDA,
+    test_LDA_reduction,
+
+    test_naive,
+
+    test_NeuralNetwork,
+
+    test_svm,
+
+    test_GaussianMixture,
+
+    test_kmeans_random,
+    test_kmeans_speed,
+
+    test_PCA,
+
+    test_spec_cluster,
+
+    test_Adaboost,
+    test_BoostingTree,
+    test_randomForest,
+
+    make_cla,
+    make_reg,
+    make_digits,
+    make_blo,
+    make_iris,
+
+    train!,
+    predict,
+
+    RegressionTree,
+    ClassificationTree,
+    randomForest,
+    Adaboost,
+    LinearRegression,
+    LogisticRegression,
+    GDA,
+    BoostingTree,
+    KnnRegression,
+    KnnClassifier,
+    label_propagation,
+    show_example,
+    LDA,
+    plot_in_2d,
+    NaiveBayes,
+    NeuralNetwork,
+    svm,
+    GaussianMixture,
+    Kmeans,
+    PCA,
+    transform,
+    spec_clustering
+
+
+
+
+Features = Union{String, Real}
+
+
+
+#Supervised_learning 
+
+include("supervised_learning/baseRegression.jl")
+include("supervised_learning/decisionTree.jl")
+include("supervised_learning/gaussianDiscriminantAnalysis.jl")
+include("supervised_learning/hiddenMarkovModel.jl")
+include("supervised_learning/kNearestNeighbors.jl")
+include("supervised_learning/labelPropagation.jl")
+include("supervised_learning/linearDiscriminantAnalysis.jl")
+include("supervised_learning/naivdBayes.jl")
+include("supervised_learning/neuralNetwork_bp.jl")
+include("supervised_learning/support_vector_machine.jl")
+include("supervised_learning/adaboost.jl")
+include("supervised_learning/randomForests.jl")
+include("supervised_learning/xgboost.jl")
+include("supervised_learning/GradientBoostingTree.jl")
+
+
+#Unsupervised_learning 
+
+include("unsupervised_learning/gaussianMixtureModel.jl")
+include("unsupervised_learning/kMeans.jl")
+include("unsupervised_learning/principalComponentAnalysis.jl")
+include("unsupervised_learning/spectralCluster.jl")
+include("unsupervised_learning/largeScaleSpectralClustering.jl")
+
+#Utils
+
+include("utils/utils.jl")
+include("utils/demo.jl")
+
+
+
+
+
+end
---
+++ utils/frame.jl
@@ -0,0 +1,50 @@
+
+type ModelName
+    
+end
+
+function ModelName()
+
+end
+
+
+function train!(model::ModelName, X::Matrix, y::Vector)
+
+end
+
+function predict(model::ModelName, 
+                 x::Matrix)
+    n = size(x,1)
+    res = zeros(n)
+    for i = 1:n 
+        res[i] = predict(model, x[i,:])
+    end
+    return res
+end
+
+function predict(model::ModelName,
+                 x::Vector)
+end
+
+
+
+function test_ModelName()
+    X_train, X_test, y_train, y_test = make_cla()
+    model = ModelName()
+    train!(model,X_train, y_train)
+    predictions = predict(model,X_test)
+    print("classification accuracy", accuracy(y_test, predictions))
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
