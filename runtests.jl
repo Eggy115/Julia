f8830780a50ae3dca9e106e93b9a7aa2f6f7f97f
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-09 10:04:28 +0100

    Add files via upload

--- runtests.jl
+++ runtests.jl
@@ -1,18 +1,5 @@
-using Test
+using LightML
+using Base.Test
 
-function print_evalfile(fdir, fname)
-    println("EVALUATING ", fname, ":\n*************")
-    result = evalfile(joinpath(fdir, fname))
-    println("*************\n")
-    return result
-end
-
-fdir, this_fname = splitdir(@__FILE__)
-fdir = joinpath(fdir, "../src")
-cd(fdir)
-
-for fname in readdir(".")
-    if endswith(fname, ".jl") && fname != this_fname
-        print_evalfile(fdir, fname)
-    end
-end
+# write your own tests here
+@test 1 == 1
